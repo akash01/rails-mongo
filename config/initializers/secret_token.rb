0351a8184963a939bb4256b1fe8704fd5a774c81
mukesh <pace.akash@gmail.com>
2014-03-06 00:18:44 +0200

    Initital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mongoblog::Application.config.secret_key_base = 'd13265cded511c6001462d91db4d8e522c2eb417cc48f244699b2dd9bc489e7747b4a9a1ccc593fd577392ce68a60dc48934393303d77b73c0cb8e8e81ae5a35'
