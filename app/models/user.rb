1f65b939b0499c73fd257112c63c19097f44bd4d
mukesh <pace.akash@gmail.com>
2014-03-06 01:46:21 +0200

    added basic user model

---
+++ app/models/user.rb
@@ -0,0 +1,26 @@
+class User
+  include Mongoid::Document
+  include ActiveModel::SecurePassword
+  include Mongoid::Timestamps::Created
+  include Mongoid::Timestamps::Updated
+
+  field :firstname, type: String
+  field :lastname, type: String
+  field :password_digest, type: String
+  field :email, type: String 
+  
+  index({ email: 1 }, { unique: true })
+
+  before_save { self.email = email.downcase }
+  has_secure_password
+
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :firstname, presence: true, length: { maximum: 50 }
+  validates :lastname, presence: true, length: { maximum: 50 }
+  
+  validates :email, presence: true, format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
+  validates :password, length: { minimum: 6 }
+
+end
